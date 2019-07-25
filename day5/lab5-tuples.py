53bba00239db79b449c20024574c8aa15f45c4a5
simon21-meet <simon21@meet.mit.edu>
2019-07-25 09:55:41 -0400

    Stopping here

---
+++ day5/lab5-tuples.py
@@ -0,0 +1,12 @@
+x,y,z=(3,4,5)
+my_tuple=(3,4,5)
+print(x+y+z)
+print(my_tuple+my_tuple)
+print(x*3)
+print(my_tuple*3)
+print(my_tuple[0]==x)
+x=17
+print(x)
+import turtle
+print(turtle.pos())
+print(type(turtle.pos()))
